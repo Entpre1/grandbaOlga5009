2105c1218e7538f6fb931dccb936e475f7f04f1e
Entpre1 <preewent@proton.me>
2025-11-25 13:11:06 +0300

    Update tablespy.py

--- tablespy.py
+++ tablespy.py
@@ -1,9 +1,16 @@
 from prettytable import PrettyTable as МилаяТаблица
 печатать = print
 лен = len
+инт = int
+вводотпользователя = input
 чек = МилаяТаблица()
-чек.field_names = ['№', 'Продукт', "Цена", "Количество", "Стоимость"]
+чек.field_names = ['№', 'product', "price", "quantity", "finalprice"]
+
+for i in range(3):
+    продукт = вводотпользователя('enter product name')
+    цена = инт(вводотпользователя('enter product price'))
+    количество = инт(вводотпользователя('enter product quantity'))
+    чек.add_row([i+1, продукт, f'{цена}', f'{количество}', f'{цена*количество}'])
+    
 
 печатать(чек)
-
-
