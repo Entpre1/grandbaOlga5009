a45a58bc9ea96088feb8f4b3ee17f887ea5412a7
Entpre1 <preewent@proton.me>
2025-11-25 13:23:19 +0300

    Update tablespy.py

--- tablespy.py
+++ tablespy.py
@@ -1,17 +1,12 @@
 from prettytable import PrettyTable
 
-print_func = print
-length = len
-to_int = int
-user_input = input
-
 table = PrettyTable()
 table.field_names = ['№', 'product', "price", "quantity", "finalprice"]
 
 for i in range(3):
-    product = user_input('enter product name')
-    price = to_int(user_input('enter product price'))
-    quantity = to_int(user_input('enter product quantity'))
+    product = input('enter product name')
+    price = int(input('enter product price'))
+    quantity = int(input('enter product quantity'))
     table.add_row([i+1, product, f'{price}', f'{quantity}', f'{price * quantity}'])
 
-print_func(table)
+print(table)
